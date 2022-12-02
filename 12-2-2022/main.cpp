65c2476c1ccb9c2d8287aa55f95d92ca1ec48633
Jacob M <jmielc2@uic.edu>
2022-12-02 13:46:09 -0600

    Day 2

---
+++ 12-2-2022/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+#define WIN 6
+#define DRAW 3
+
+enum {X=0,Y=3,Z=6};
+enum {A=1,B=2,C=3};
+
+int isWinner(const char &a, const char &b) {
+    int p1 = a - 'A';
+    int p2 = b - 'A';
+    
+    if (p2 - p1 == 1 || (p2 == 0 && p1 == 2)) {
+        return 2;
+    } else if (p1 - p2 == 1 || (p1 == 0 && p2 == 2)) {
+        return 1;
+    }
+    return 0;
+}
+
+int getPlay(const int &opp, const int &outcome) {
+    int play;
+    if (outcome == Z) {
+        if (opp == C) {
+            play = A;
+        } else {
+            play = opp + 1;
+        }
+    } else if (outcome == X) {
+        if (opp == A) {
+            play = C;
+        } else {
+            play = opp - 1;
+        }
+    } else {
+        play = opp;
+    }
+    return play + outcome;
+}
+
+int main() {
+    string filename = "input.txt";
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error: Failed to open " << filename << "." << endl;
+        return -1;
+    }
+
+    int round, score = 0;
+    string input;
+    while (!file.eof()) {
+        round = 0;
+        getline(file, input);
+        if (input == "") {
+            continue;
+        }
+        int opp = input[0] - 'A' + 1;
+        switch (input[2]) {
+        case('X'):
+            round += getPlay(opp, X);
+            break;
+        case('Y'):
+            round += getPlay(opp, Y);
+            break;
+        case('Z'):
+            round += getPlay(opp, Z);
+            break;
+        }
+        // cout << input << " -> " << round << endl;
+        score += round;
+    }
+
+    cout << "Final Score: " << score << endl;
+    return 0;
+}
