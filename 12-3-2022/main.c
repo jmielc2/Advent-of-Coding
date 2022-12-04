81c9afee860f585b050ec2cac31c02ac8f719cd8
Jacob M <jmielc2@uic.edu>
2022-12-04 13:05:09 -0600

    Day 3 (with C)

---
+++ 12-3-2022/main.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define BUF_SIZE 64
+
+typedef struct {
+    char *buf;
+    int size;
+} string;
+
+string initString() {
+    string a;
+    a.buf = NULL;
+    a.size = 0;
+    return a;
+}
+
+void destroyString(string a) {
+    if (a.buf) {
+        free(a.buf);
+    }
+}
+
+typedef struct {
+    FILE* file;
+    string* input;
+    char delim;
+} f_getline_args;
+
+int f_getline_base(FILE* file, string *input, char delim) {
+    if (input->buf != NULL) {
+        free(input->buf);
+    }
+    input->size = 0;
+    char buf[BUF_SIZE], a;
+    int size = 0;
+    while ((a = fgetc(file)) != EOF && a != delim) {
+        if (size == BUF_SIZE) {
+            fprintf(stderr, "BUF_SIZE is too small.\n");
+            return 0;
+        }
+        buf[size] = a;
+        size++;
+    }
+    input->buf = malloc(sizeof(char) * (size + 1));
+    if (!input->buf) {
+        perror("malloc failed to allocate space.");
+        return 0;
+    }
+    memcpy(input->buf, buf, size);
+    input->buf[size] = '\0';
+    input->size = size;
+    return 1;
+}
+
+int f_getline_var(f_getline_args in) {
+    char delim = (in.delim)? in.delim : '\n';
+    return f_getline_base(in.file, in.input, delim);
+}
+
+#define getline(...) f_getline_var((f_getline_args){__VA_ARGS__})
+
+string substr(const char* buf, int index, int size) {
+    string a = initString();
+    a.size = size;
+    a.buf = malloc(sizeof(char) * (size + 1));
+    for (int i = 0; i < size; i++) {
+        a.buf[i] = buf[index + i]; 
+    }
+    a.buf[size] = '\0';
+    return a;
+}
+
+int getPriority(const char a) {
+    if (isupper(a)) {
+        return a - 'A' + 27;
+    }
+    return a - 'a' + 1;
+}
+
+int main(int argc, char* argv[]) {
+    char* filename = "input.txt";
+    if (argc == 2) {
+        filename = argv[1];
+    } else if (argc > 2) {
+        fprintf(stderr, "Usage: %s [<filename>]\n", argv[0]);
+        return -1;
+    }
+
+    FILE* file = fopen(filename,  "r");
+    if (!file) {
+        fprintf(stderr, "Failed to open '%s'.\n", filename);
+        return -1;
+    }
+
+    int sum = 0;
+    string input = initString();
+    char items[26 * 2], temp[26 * 2], counts[26 * 2];
+    while (!feof(file)) {
+        if (!getline(file, &input)) {
+            fprintf(stderr, "Error: getline function failed.\n");
+            return -1;
+        }
+        if (!strcmp("", input.buf)) {
+            continue;
+        }
+        memset(&items, 0, sizeof(items));
+        memset(&counts, 0, sizeof(counts));
+        for (int i = 0; i < input.size; i++) {
+            int index = getPriority(input.buf[i]) - 1;
+            items[index] = 1;
+            counts[index] = 2;
+        }
+        for (int i = 0; i < 2; i++) {
+            memcpy(&temp, &items, sizeof(items));
+            if (!getline(file, &input)) {
+                fprintf(stderr, "Error: getline function failed.\n");
+                return -1;
+            }
+            for (int j = 0; j < input.size; j++) {
+                int index = getPriority(input.buf[j]) - 1;
+                if (temp[index]) {
+                    counts[index]--;
+                    temp[index] = 0;
+                    if (!counts[index]) {
+                        sum += getPriority(input.buf[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("Total: %d\n", sum);
+    destroyString(input);
+    return 0;
+}
